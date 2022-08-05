653dff836f245d1e9f97e6b9c4058dae757595e0
Sagar <Sagar@LAPTOP-G635REBJ>
2022-08-05 10:00:46 +0530

    Hello

--- zerodha/src/main/java/baseClasses/BaseClass.java
+++ zerodha/src/main/java/baseClasses/BaseClass.java
@@ -1,5 +1,18 @@
 package baseClasses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import configurations.Configuration;
+
 public class BaseClass {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", Configuration.driverPath);
+		WebDriver driver = new ChromeDriver();
+		driver.get(Configuration.appUrl);
+				
+		driver.manage().window().maximize();
+		driver.quit();
+	}
 
 }
